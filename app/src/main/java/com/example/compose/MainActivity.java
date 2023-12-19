271a2c5b5891d0a0c38d66f8a86007c9e78665fd
WGQZYJZ <1074781857@qq.com>
2023-12-19 16:51:13 +0800

    美化界面

--- app/src/main/java/com/example/compose/MainActivity.java
+++ app/src/main/java/com/example/compose/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.compose;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import android.app.Activity;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    private TextView title;
     private Button btnGoToLogin;
     private Button btnGoToSignIn;
 
